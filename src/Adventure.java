53b94fc38bcc6be3fc7367d73cc14788ca79f917
BT_4N2_22 <ahelik4984@bths.edu>
2022-11-15 12:34:12 -0500

    added character objects and info method

--- src/Adventure.java
+++ src/Adventure.java
@@ -2,11 +2,28 @@ import java.util.Scanner;
 public class Adventure {
     public static void main(String[] args) {
         Scanner answer = new Scanner(System.in);
-        String yourClass;
+        String yourClass = "";
+        Character yourCharacter = null;
         System.out.println("Welcome to your adventure!");
         System.out.println("Lets create your character!");
         System.out.println("What class do you want to pick? Enter 1 for warrior, 2 for mage, or 0 if you want character creation to be randomized.");
-        int classPick = answer.nextInt();
-        System.out.println("Your adventure will be in the " + );
+        int classPick = Integer.parseInt(answer.nextLine());
+        if (classPick == 0){
+            Character character = new Character();
+            yourCharacter = character;
+        }
+        else if (classPick == 1){
+            System.out.println("You picked the warrior! What do you want to be called?");
+            String yourName = answer.nextLine();
+            Character character = new Character(1, yourName);
+            yourCharacter = character;
+        }
+        else if (classPick == 2){
+            System.out.println("You picked the mage! What do you want to be called?");
+            String yourName = answer.nextLine();
+            Character character = new Character(2, yourName);
+            yourCharacter = character;
+        }
+        System.out.println(yourCharacter.info());
     }
 }
--- src/Character.java
+++ src/Character.java
@@ -5,16 +5,55 @@ public class Character {
 
     public Character(int yourClass , String name){
         chosenName = name;
+        if (yourClass == 1){
+            chosenClass = "Warrior";
+        }
+        else if (yourClass == 2){
+            chosenClass = "Mage";
+        }
     }
     public Character(){
-        int classPick = ((int)Math.random()*2)+1;
+        int classPick = (int)(Math.random()*2)+1;
         if(classPick == 1){
             chosenClass = "Warrior";
         }
         else{
             chosenClass = "Mage";
         }
-        int namePick = ((int)Math.random()*10)+1;
+        int namePick = (int)(Math.random()*10)+1;
+        if (namePick == 1){
+            chosenName = "John";
+        }
+        if (namePick == 2){
+            chosenName = "Joanna";
+        }
+        if (namePick == 3){
+            chosenName = "Quinn";
+        }
+        if (namePick == 4){
+            chosenName = "Steven";
+        }
+        if (namePick == 5){
+            chosenName = "Eddy";
+        }
+        if (namePick == 6){
+            chosenName = "Leon";
+        }
+        if (namePick == 7){
+            chosenName = "Claire";
+        }
+        if (namePick == 8){
+            chosenName = "Rachel";
+        }
+        if (namePick == 9){
+            chosenName = "Flora";
+        }
+        if (namePick == 10){
+            chosenName = "Rosa";
+        }
+    }
+    public String info(){
+        return "Your character's name is " + chosenName + ", and your class is " + chosenClass;
     }
     public String location(){
         String thisLocation;
